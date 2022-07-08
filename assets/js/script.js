2b4e2f6be81e4d602efff2ef55f19550e99b170c
Farah M <farah.maria.rahman@gmail.com>
2022-07-08 23:12:30 +0000

    Add questions to array in JS file.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,47 @@
 const question = document.querySelector('#question');
 const choices = Array.from(document.querySelectorAll('.choice-text'));
 
+
+let currentQuestion = {}
+let acceptingAnswers = true
+let availableQuestions = []
+
+let questions = [
+    {
+        question: "apple",
+        choice1: "noun",
+        choice2: "adjective",
+        choice3: "verb",
+        choice4: "adverb",
+        answer: 1,
+    },
+    {
+        question:"round",
+        choice1: "noun",
+        choice2: "adjective",
+        choice3: "verb",
+        choice4: "adverb",
+        answer: 2,
+    },
+    {
+        question: "quickly",
+        choice1: "noun",
+        choice2: "adjective",
+        choice3: "verb",
+        choice4: "adverb",
+        answer: 4,
+    },
+    {
+        question: "rolled",
+        choice1: "noun",
+        choice2: "adjective",
+        choice3: "verb",
+        choice4: "adverb",
+        answer: 3,
+    }
+]
+
+
 /*
 
 let score = 0;
